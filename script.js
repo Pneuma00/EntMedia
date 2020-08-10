04e025c06bf13506007f92a2d2ff78aa227b74d5
Pneuma <syca0714@gmail.com>
2020-08-10 14:14:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ content.innerHTML = content.innerHTML
         match => `<img src="${match.slice(15, match.indexOf('" target'))}"></img>`
     ).replace(
         /Youtube:<a href="https:\/\/youtu.be\/[a-zA-Z0-9-_]+" target="_blank">https:\/\/youtu.be\/[a-zA-Z0-9-_]+<\/a>/g,
-        match => `<iframe width="640" height="480" src="https://www.youtube.com/embed/${match.slice(17)}"></iframe>`
+        match => `<iframe width="640" height="360" frameborder=0 src="https://www.youtube.com/embed/${match.slice(34, match.indexOf('" target'))}"></iframe>`
     )
